df8ba3bad0ef3a1e464a8a5cadbbbcf034ca923f
Damir Kotoric <damir.kotoric@envato.com>
2016-05-28 17:20:10 +1000

    hero section responsive but yet to be thorougly tested

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,12 +2,13 @@ $(function() {
   // Init
   var iframe = $('#player1')[0]
   var player = $f(iframe)
+
+  // Listeners
   player.addEvent('ready', function() {
     player.addEvent('playProgress', onPlayProgress)
   })
+  $("#mc-embedded-subscribe-form").on("submit", formSubmitting)
 })
-
-// Listeners
 
 var videoIsShown = false
 function onPlayProgress(data) {
@@ -15,5 +16,13 @@ function onPlayProgress(data) {
   if (!videoIsShown) {
     $(".hero__video").delay(5000).addClass("-visible")
     videoIsShown = true
+  }
+}
+
+function formSubmitting(event) {
+  $emailInput = $("#mce-EMAIL")
+  if (!$emailInput.val()) {
+    event.preventDefault()
+    $emailInput.focus()
   }
 }
