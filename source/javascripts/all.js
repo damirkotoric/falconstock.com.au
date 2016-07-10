519a41b02fa60b5bf971a5f42c30cee57f5d2d32
Damir Kotoric <damirkotoric@gmail.com>
2016-07-10 20:12:29 +1000

    added awesome Fitsy copy

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,56 +3,50 @@ $(function() {
   var iframe = $('#player1')[0]
   var player = $f(iframe)
 
-  // Stripe init
-  var handler = StripeCheckout.configure({
-    key: 'pk_test_jLS9nbug24AGz4aGzQSReQQT',
-    image: '/img/documentation/checkout/marketplace.png',
-    locale: 'auto',
-    token: function(token) {
-      // You can access the token ID with `token.id`.
-      // Get the token ID to your server-side code for use.
-    }
-  });
+  // // Stripe init
+  // var handler = StripeCheckout.configure({
+  //   key: 'pk_test_jLS9nbug24AGz4aGzQSReQQT',
+  //   image: '/img/documentation/checkout/marketplace.png',
+  //   locale: 'auto',
+  //   token: function(token) {
+  //     // You can access the token ID with `token.id`.
+  //     // Get the token ID to your server-side code for use.
+  //     $(".hero__button").hide()
+  //     $(".hero__confirmation").addClass("-visible")
+  //     $(".hero__confirmation-name").text(token.card.name)
+  //     $(".hero__confirmation-email").text(token.email)
+  //   }
+  // });
 
   // Listeners
   player.addEvent('ready', function() {
     player.addEvent('playProgress', onPlayProgress)
   })
-  $("#mc-embedded-subscribe-form").on("submit", formSubmitting)
 
-  // Stipe listeners
-  $('#purchase-button').on('click', function(e) {
-    // Open Checkout with further options:
-    handler.open({
-      name: 'Falcon Stock',
-      description: 'Geelong aerial footage package',
-      zipCode: true,
-      currency: "aud",
-      amount: 490000,
-      billingAddress: true,
-      image: "http://falconstock.com.au/images/falconstock-stripe-logo.jpg"
-    });
-    e.preventDefault();
-  });
-  // Close Checkout on page navigation:
-  $(window).on('popstate', function() {
-    handler.close();
-  });
+  // // Stipe listeners
+  // $('#purchase-button').on('click', function(e) {
+  //   // Open Checkout with further options:
+  //   handler.open({
+  //     name: 'Falcon Stock',
+  //     description: 'Geelong aerial footage package',
+  //     zipCode: true,
+  //     currency: "aud",
+  //     amount: 490000,
+  //     billingAddress: true,
+  //     image: "http://falconstock.com.au/images/falconstock-stripe-logo.png"
+  //   });
+  //   e.preventDefault();
+  // });
+  // // Close Checkout on page navigation:
+  // $(window).on('popstate', function() {
+  //   handler.close();
+  // });
 })
 
 var videoIsShown = false
 function onPlayProgress(data) {
-  console.log(data)
   if (!videoIsShown) {
     $(".hero__video").delay(5000).addClass("-visible")
     videoIsShown = true
-  }
-}
-
-function formSubmitting(event) {
-  $emailInput = $("#mce-EMAIL")
-  if (!$emailInput.val()) {
-    event.preventDefault()
-    $emailInput.focus()
   }
 }
